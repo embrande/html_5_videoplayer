60a803d50e788b5c29fd62376582a68fe8cc94b0
Embrande <michelegretchcarter@EricBrandenburg.attlocal.net>
2016-06-13 16:45:26 -0400

    Added comments for structure purposes

---
+++ video_player_structure.js
@@ -0,0 +1,24 @@
+var html_5_video_player_eb = (function (html_5_video_player_eb) {
+
+
+	// Attaches onto class of video container.
+
+	// Gets the controls and all videos inside of video container
+
+	// creates objects of all video containers
+
+	// ? actions of container buttons with an event listener that takes selector, object/objects item, and action
+
+	// ? add the class I want to items if I can figure them out so the user doesn't need to have pre-determined play/pause buttons etc.
+
+
+
+	//return the object created
+	return html_5_video_player_eb;
+
+//if the object 
+}(html_5_video_player_eb || {}));
+
+
+// below is what I want the user to be able to do to create the video player
+// var videoPlayer = html_5_video_player_eb.createVideoPlayer("containerID");
