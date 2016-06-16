4be4fc2193114fe3b8b872345020db8ff5b2f8c4
Embrande <michelegretchcarter@EricBrandenburg.attlocal.net>
2016-06-16 10:05:39 -0400

    Created the fundamental factory function that will create the video container based on ID given. This will then look for video tag direct decendants, wrap them in divs. Then, give all direct div descendents objects.

--- video_player_structure.js
+++ video_player_structure.js
@@ -1,16 +1,19 @@
 var html_5_video_player_eb = (function (html_5_video_player_eb) {
 
 
-	// Attaches onto class of video container.
+	var CreateVideoContainer = function(videoContainerElement){
 
-	// Gets the controls and all videos inside of video container
+	};
 
-	// creates objects of all video containers
 
-	// ? actions of container buttons with an event listener that takes selector, object/objects item, and action
+	html_5_video_player_eb.createVideoContainer = function(elementID){
 
-	// ? add the class I want to items if I can figure them out so the user doesn't need to have pre-determined play/pause buttons etc.
+		//creates video contianer based off of ID
+        var element = document.getElementById(elementID);
 
+        // Create new facotry function of video container
+        return new CreateVideoContainer(element);
+	};
 
 
 	//return the object created
@@ -21,4 +24,4 @@ var html_5_video_player_eb = (function (html_5_video_player_eb) {
 
 
 // below is what I want the user to be able to do to create the video player
-// var videoPlayer = html_5_video_player_eb.createVideoPlayer("containerID");
+// -> var videoPlayer = html_5_video_player_eb.createVideoPlayer("containerID");
